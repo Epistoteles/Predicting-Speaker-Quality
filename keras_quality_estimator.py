64945c843cd868558686339bd76c7540ef004021
Epistoteles <korbinian-koch@web.de>
2020-12-31 15:22:27 +0100

    experiment 2 setup

--- keras_quality_estimator.py
+++ keras_quality_estimator.py
@@ -29,7 +29,7 @@ CROSS_VAL = 10
 ES_PATIENCE = 2
 LOSS = "mean_squared_error"
 USE_LSTM = False
-FEATURE_TYPE = "embeddings-ge2e"  # embeddings-ge2e, embeddings-trill, feature-streams (embeddings dir name)
+FEATURE_TYPE = "embeddings-trill"  # embeddings-ge2e, embeddings-trill, feature-streams (embeddings dir name)
 FEATURE_DIR = "split-10"  # split-10, ... (subdir name in ./wavs)
 
 seed = random.randint(0, 100000)
